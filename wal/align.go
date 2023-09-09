9e6418187d431bef07d3aeb0b869acbaa21f0357
krisdiano <litianxiang01@gmail.com>
2023-09-09 21:22:07 +0800

    save

--- wal/align.go
+++ wal/align.go
@@ -2,18 +2,17 @@ package wal
 
 import "io"
 
-func Align(seeker io.Seeker) error {
+func Align(seeker io.Seeker) (int64, error) {
 	offset, err := seeker.Seek(0, io.SeekCurrent)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	if offset == 0 {
-		return nil
+		return 0, nil
 	}
 	quotient, remainder := offset/8, offset%8
 	if remainder != 0 {
 		remainder = 8
 	}
-	_, err = seeker.Seek(quotient*8+remainder, io.SeekStart)
-	return err
+	return seeker.Seek(quotient*8+remainder, io.SeekStart)
 }
